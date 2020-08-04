6b812ea5d287b1660cb5685c15e4a5a04aa74e22
bartxs <b.soltysik90@gmail.com>
2020-08-04 15:57:50 +0200

    Exercise 3

---
+++ Bartosz/javascript.js
@@ -0,0 +1,58 @@
+// 0.
+
+let snackServings = 50
+let guests = 25
+
+// 1.
+
+console.log(snackServings + guests);
+
+// 2.
+
+console.log(guests - snackServings);
+console.log(snackServings - guests);
+
+// 3.
+
+console.log(snackServings * guests);
+
+// 4.
+
+console.log(snackServings / guests);
+
+// 5.
+
+let drinks = 10
+let resultOne = (snackServings * guests / drinks);
+console.log(resultOne);
+
+// 6.
+
+let monkeys = 15
+let bananas = 9
+console.log(monkeys / bananas);
+
+// .7
+
+let monkeyfights = 20
+let resoultTwo = (monkeys + bananas) * monkeyfights;
+console.log(resoultTwo);
+
+// 8.
+
+console.log(monkeys++);
+console.log(monkeys);
+
+// 9.
+
+console.log(bananas--);
+console.log(bananas);
+
+// 10.
+
+let happines = (bananas - monkeys);
+console.log(happines + monkeyfights);
+
+// 11.
+
+console.log(resultOne % resoultTwo);
