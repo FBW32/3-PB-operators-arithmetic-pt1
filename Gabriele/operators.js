207b63861c1cbc9a2a75264907374a03a1eb991f
Gabriele Pepe <gabriele.pepe@gmail.com>
2020-08-04 15:42:41 +0200

    task completed

---
+++ Gabriele/operators.js
@@ -0,0 +1,44 @@
+// Declare two variables "snackServings" and "guests" and assign them number values.
+
+let snackServings = 30;
+let guests = 20;
+
+console.log(snackServings, guests);
+console.log(snackServings + guests);
+
+console.log(snackServings - guests);
+
+console.log(guests - snackServings);
+
+console.log(snackServings * guests);
+
+console.log(snackServings / guests);
+
+let drinks = 10;
+console.log(snackServings * guests / drinks);
+let resultOne = 60;
+console.log(resultOne);
+
+let monkeys = 15;
+let bananas = 9;
+console.log(monkeys / bananas);
+
+let monkeyfights = 20;
+console.log((monkeys + bananas) * monkeyfights);
+let resultTwo = 480;
+console.log(resultTwo);
+
+
+monkeys++;
+console.log(monkeys++);
+
+bananas--;
+console.log(bananas--);
+
+console.log(bananas - monkeys);
+
+let happiness = -10;
+console.log(happiness + monkeyfights);
+
+//console.log(resultOne / resultTwo);
+console.log(0);
