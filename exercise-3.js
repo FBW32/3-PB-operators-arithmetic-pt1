0ffa02c73258b106508e5e9d52fd76fecae79909
frausafiye <s_gulenc@outlook.com>
2020-08-05 10:26:05 +0200

    added solutions

---
+++ exercise-3.js
@@ -0,0 +1,36 @@
+//1
+let snackServings = 4;
+let guests = 5;
+
+console.log(snackServings);
+console.log(guests);
+//2
+console.log(guests - snackServings);
+console.log(snackServings - guests);
+//3
+console.log(guests * snackServings);
+//4
+console.log(snackServings / guests);
+//5
+let drinks = "10";
+//let resultOne = (guests * snackServings) / drinks; or:
+resultOne = (guests * snackServings) / drinks;
+console.log(resultOne);
+//6
+let monkeyfights = 20;
+let monkeys = 10;
+let bananas = 50;
+resultTwo = (monkeys + bananas) / monkeyfights;
+console.log(resultTwo);
+//7
+monkeys = monkeys + 10;
+console.log(monkeys);
+//8
+bananas = bananas - 25;
+console.log(bananas);
+//9
+happiness = bananas - monkeys;
+monkeyfights = monkeyfights + happiness;
+console.log(monkeyfights);
+//10
+console.log(resultOne % resultTwo);
