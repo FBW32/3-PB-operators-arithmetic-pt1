bdd2f32acbb3d1460ddf596828f6799b9951b3fd
anika555 <ak15@live.de>
2020-08-05 16:09:11 +0200

    Exercise 3

---
+++ anika/jsexe3.js
@@ -0,0 +1,75 @@
+// *************Exercise 3*******************
+
+// 0. Declare two variables "snackServings" and "guests"
+// and assign them number values.
+
+let snackServings = 2;
+let guests = 3;
+
+// 1. Add snackServings and guests.
+
+console.log(snackServings + guests); // 5
+
+// 2. Subtract guests from snackServings. Then, subtract snackServings from guests.
+
+console.log(snackServings - guests); // -1
+console.log(guests - snackServings); // 1
+
+// 3. Multiply snackServings and guests.
+
+console.log(snackServings * guests); // 6
+
+// 4. Divide snackServings by guests.
+
+console.log(snackServings / guests); // 0.6666666666666666
+
+// 5. Declare another variable "drinks" with value "10".
+// Multiply snackServings and guests. Then, divide the result by drinks.
+// Store the result in a new variable named "resultOne".
+// Print "resultOne" to the console.
+
+let drinks = 10;
+
+console.log(drinks * snackServings * guests); // 60
+console.log(drinks / 60); // 0.16666666666666666
+
+let resultOne = 0.16666666666666666;
+
+console.log(resultOne); // 0.16666666666666666
+
+// 5. Declare two variables:
+// "monkeys" with value 15, and "bananas" with value 9. Divide monkeys by bananas.
+
+let monkeys = 15;
+let bananas = 9;
+
+console.log(monkeys / bananas); // 1.6666666666666667
+
+// 6. Declare another variable "monkeyfights" with value 20.
+// Add monkeys and bananas, then multiply the result by monkeyfights.
+// Store the result in variable "resultTwo".
+// Print "resultTwo" to the console.
+
+let monkeyfights = 20;
+
+console.log(monkeys + bananas); // 24
+console.log(24 * monkeyfights); // 480
+
+let resultTwo = 480;
+
+console.log(resultTwo); // 480
+
+// 7. Increment monkeys.
+
+monkeys++;
+console.log(monkeys); // 16
+
+// 8. Decrement bananas.
+
+bananas--;
+console.log(bananas); // 8
+
+// 10. Print the remainder when "resultOne" is divided by "resultTwo"
+// to the console.
+
+console.log(resultOne % resultTwo); // 0.16666666666666666
