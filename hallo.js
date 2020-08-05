1e800b2d0a0a5f8dc6af0c42adb941a03364c9a8
Maja <majamarek.design@gmail.com>
2020-08-05 14:14:40 +0200

    rrready!

---
+++ hallo.js
@@ -0,0 +1,23 @@
+let snackServings = 1;
+let guests = 2;
+console.log(snackServings + guests);
+console.log(guests - snackServings);
+console.log(snackServings * guests);
+console.log(snackServings / guests);
+let drinks = 10;
+console.log((snackServings * guests) / drinks);
+let resultOne = 0.2;
+console.log(resultOne);
+let monkeys = 15;
+let bananas = 9;
+console.log(monkeys / bananas);
+let monkeyfights = 20;
+console.log((monkeys + bananas) * monkeyfights);
+/* let resultTwo = 480;
+resultTwo++;
+console.log("Result is " + resultTwo); */
+monkeys++;
+console.log("The result is " + monkeys);
+
+bananas--;
+console.log("The result is " + bananas);
