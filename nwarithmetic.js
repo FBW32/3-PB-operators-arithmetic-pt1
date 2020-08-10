e5c96134fe17c661bc4a5a811356b68dd045f405
LawrenzoDM <lawrenzodelmc@outlook.com>
2020-08-10 16:22:08 +0200

    addding math

---
+++ nwarithmetic.js
@@ -0,0 +1,48 @@
+let snackServings = 25;
+let guests = 40;
+
+//1.)
+
+console.log(snackServings + guests);  //65
+
+//2.)
+console.log(guests - snackServings); // 15
+console.log(snackServings - guests);  //-15
+
+//3.)
+console.log(snackServings * guests); //1000
+//4.)
+console.log(guests / snackServings);  //1.6
+
+//5.)
+let drinks = 10;
+console.log((snackServings * guests) / drinks ); // 100
+
+let resultOne = 100;
+console.log(resultOne);  // 100
+
+let monkeys = 15;
+let bananas = 9;
+console.log(monkeys / bananas);  //1.6666666666666667
+
+//6.)
+let monkeyFights = 20;
+console.log((monkeys + bananas) * monkeyFights); //480
+
+let resultTwo = 480;
+
+//7.)
+monkeys++;
+console.log("monkey is " + monkeys);  // monkey is 16
+
+//8.)
+bananas--;
+console.log("bananas is " + bananas); // banana is 8
+
+console.log(monkeys - bananas);  // 8
+let happiness = 8;
+
+console.log(happiness + monkeyFights); //28
+
+console.log(resultOne % resultTwo); // 100
+ 
